b9c66b2dda751ae0922f6a62f0f3729fac38d0b3
diegoh2525 <jdherrera.2525@gmail.com>
2025-05-12 11:07:30 -0500

    Entidades y Repositorios añadidos

--- ProyectoFinalJDH/src/main/java/com/corhuila/ProyectoFinalJDH/Repository/ReservaRepository.java
+++ ProyectoFinalJDH/src/main/java/com/corhuila/ProyectoFinalJDH/Repository/ReservaRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 import com.corhuila.ProyectoFinalJDH.Entity.Reserva;
 
 @Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long>{
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    // Aquí puedes agregar métodos personalizados si es necesario
+    // Por ejemplo, para buscar reservas por usuario, fecha, etc.
+    // List<Reserva> findByUsuario(Usuario usuario);
 
 }
